c42ae572699d806f5e81f28b9314ba9914c5a062
Connor Kirkwood <ckirkwood7464@gmail.com>
2019-12-13 15:38:07 -0500

    Sequelize should be done

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const express = require('express')
 const app = express()
-const teams = require('./teams.json')
+//const teams = require('./teams.json')
 const bodyParser = require('body-parser')
-// const models = require('./models')
-app.use(express.static('client'))
+const models = require('./models')
+//app.use(express.static('client'))
 
 app.get('/', (request, response) => {
     response.send('./client/index.html')
 })
 
 app.get('/api/teams', (request, response) => {
+    models.teams.findAll({ include: { model: models.teams}}).then((teams) => {
     response.send(teams)
+    })
 })
 
 app.get('/api/teams/:x', (request, response) => {
-    const result = teams.filter(team => request.params.x == team.id || request.params.x == team.abbreviation);
-
-    if (result.length) {
-        response.send(result)
-    } else {
-        response.sendStatus(404)
-    }
+    models.teams.findOne({
+        where: { x: request.params.x },
+        include: { model: models.teams }
+    }).then((team) => {
+        if (team) {
+            response.send(team)
+        } else {
+            response.sendStatus(404)
+        }
+    })
+    //const result = teams.filter(team => request.params.x == team.id || request.params.x == team.abbreviation);
+    // if (result.length) {
+    //     response.send(result)
+    // } else {
+    //     response.sendStatus(404)
+    // }
 })
 
 // app.get('/teams/:abbreviation', (request, response) => {
@@ -47,11 +58,19 @@ console.log(request.body)
     if (!id || !location || !mascot || !abbreviation || !conference || !division) {
         response.status(400).send('Test')
     }
+    models.teams.findOne( { where: {id: location }}).then((team) => {
+        if (!team) {
+            response.sendStatus(404).send('Unknown Team Slug')
+        } else {
+            models.teams.create({id, location, mascot, abbreviation, conference, division}).then((newTeam) => {
+                response.status(201).send(newTeam)
+            })
+        }
+    })
+    // const newTeam = {id, location, mascot, abbreviation, conference, division}
 
-    const newTeam = {id, location, mascot, abbreviation, conference, division}
-
-    teams.push(newTeam)
-    response.status(201).send(newTeam)
+    // teams.push(newTeam)
+    // response.status(201).send(newTeam)
 })
 
 app.all('*', (request, response) => {
